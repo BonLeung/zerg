4e9217f9f2ea10c671364bd4b3564a3ca3ed146b
BonLeung <654592397@qq.com>
2017-09-01 22:54:38 +0800

    commit

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -26,6 +26,9 @@ class Pay extends BaseController {
     }
 
     public function receiveNotify() {
-
+        // 1. 检测库存量，超卖
+        // 2. 更新这个订单的status状态
+        // 3. 减库存
+        // 如果成功处理，我们返回微信成功处理的信息，否则，我们需要返回没有成功处理
     }
 }
